c5fbff95aa5b3b74c8bbef8f82a6d4da602cba19
mlordSwe <alexander.csore@gmail.com>
2025-04-21 12:29:24 +0200

    Remove unused App component and styles; add Header, HomePage, and NotFound components

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,22 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.scss";
-import App from "./App.tsx";
+
 import { createBrowserRouter } from "react-router";
 import Library from "./routes/Library.tsx";
 import { Profile } from "./routes/Profile.tsx";
+import { RouterProvider } from "react-router";
+import HomePage from "./routes/HomePage.tsx";
+import { NotFound } from "./routes/NotFound.tsx";
 
 const router = createBrowserRouter([
-	{ path: "/", element: <App /> },
+	{ path: "/", element: <HomePage />, errorElement: <NotFound /> },
 	{ path: "/library", element: <Library /> },
 	{ path: "/profile", element: <Profile /> },
 ]);
 
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
-		<App />
+		<RouterProvider router={router} />
 	</StrictMode>
 );
---
+++ src/routes/NotFound.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Link } from "react-router";
+export const NotFound = () => {
+	return (
+		<div>
+			<h2>404 Not Found</h2>
+			<Link to="/">Home</Link>
+		</div>
+	);
+};
